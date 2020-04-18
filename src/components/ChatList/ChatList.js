216ae29f7a4dde1df95a5f72dd696da01bccbb9e
ipmanlk <io@navinda.xyz>
2020-04-18 14:11:39 +0530

    fix: chat is not scrolling to bottom

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -1,8 +1,30 @@
 import React from "react";
 import MsgBubble from "../MsgBubble/MsgBubble";
 
-function ChatList(props) {
-    return props.chats.map((chat, i) => <MsgBubble key={i} type={chat.type} msg={chat.msg}/>)
+class ChatList extends React.Component {
+    // reference to the last msg
+    messagesEndRef = React.createRef();
+
+    componentDidUpdate() {
+        if (this.messagesEndRef.current) this.scrollToBottom();
+    }
+
+    scrollToBottom = () => {
+        this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.chats.map((chat, i) => <MsgBubble key={i} type={chat.type} msg={chat.msg} />)}
+
+                <div style={{ float: "left", clear: "both" }}
+                    ref={this.messagesEndRef}>
+                </div>
+            </div>
+
+        )
+    }
 }
 
 export default ChatList;
