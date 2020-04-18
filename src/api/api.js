c41bb9b99df1cd2512fc57d72267150f0f4ad0af
ipmanlk <io@navinda.xyz>
2020-04-18 21:05:26 +0530

    add: api file to handle api calls

---
+++ src/api/api.js
@@ -0,0 +1,34 @@
+const getResponse = (msg) => {
+    const primaryAPI = "https://s1.navinda.xyz/ciri_bot/interact/";
+    const fallbackAPI = "https://some-random-api.ml/chatbot?message=";
+
+    return new Promise((resolve, reject) => {
+        sendRequest(primaryAPI, msg).then(res => {
+            resolve(res);
+        })
+        .catch((e) => {
+            sendRequest(fallbackAPI, msg).then(res => {
+                resolve(res);
+            }).catch(e => {
+                reject("Sorry. I didn't quite understand that.");
+            });
+        });
+    });
+}
+
+const sendRequest = (URL, msg) => {
+    return new Promise((resolve, reject) => {
+        fetch(`${URL}${msg}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                resolve(data.response);
+            })
+            .catch(e => {
+                reject(e);
+            })
+    });
+}
+
+export default getResponse;
